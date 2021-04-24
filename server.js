b42828e86bbcdd1d58a8cb57354450141bce7f0a
Leon Micheel-Sprenger <l.micheelsprenger@gmail.com>
2021-04-24 14:16:27 +0100

    9th commit: server created

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mysql = require('mysql');
 const app = express()
 const port = 3000
 
 app.use(express.static('public'));
-
 
 // parse application/x-www-form-urlencoded --> we need this to encrypt the data coming from the httpPost in the script
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+
+
+// connect to database
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'solaralliances',
+    port: '3306'
+});
+
+db.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + db.threadId);
+});
+
+
+
+
+
+//Server listening is always on the bottom!
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
